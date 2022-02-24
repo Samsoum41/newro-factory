59922056b365cea8a2713bc2a3260f90f4971c87
Samy Hocine <shocine@excilys.com>
2022-02-24 11:41:16 +0100

    Passing db confidential accesses into property file

--- src/main/java/com/samsoum/newro/persistence/DatabaseConnection.java
+++ src/main/java/com/samsoum/newro/persistence/DatabaseConnection.java
@@ -1,21 +1,41 @@
 package com.samsoum.newro.persistence;
 
-import java.sql.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.samsoum.newro.util.UtilitaireProgram;
 
 public class DatabaseConnection {
     private Connection con;
-    static final String URL = "jdbc:mysql:// localhost:3306/newro-factory-db";
-    static final String USERNAME = "adminnewro";
-    static final String PASSWORD = "qwerty1234";
+    String URL;
+    String USERNAME;
+    String PASSWORD;
     
     private DatabaseConnection()
     {
-        try {
+    	try(InputStream propertiesFile = new FileInputStream(UtilitaireProgram.WORKING_PATH + "src/main/resources/config.properties")) {
+        	Properties prop = new Properties();
+        	prop.load(propertiesFile);
+        	URL = prop.getProperty("db.url");
+        	USERNAME = prop.getProperty("db.username");
+        	PASSWORD = prop.getProperty("db.password");
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         }
         catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+        }
+        catch(FileNotFoundException e) {
+        	e.printStackTrace();
+        }
+        catch(IOException e) {
+        	e.printStackTrace();
         }
     }
     public static Connection getConnection()
---
+++ src/main/java/com/samsoum/newro/util/UtilitaireProgram.java
@@ -0,0 +1,8 @@
+package com.samsoum.newro.util;
+
+public class UtilitaireProgram {
+	public final static String WORKING_PATH = "/home/oxyl/Documents/Formation/newro/mynewrofactory/";
+	private UtilitaireProgram() {
+		
+	}
+}
